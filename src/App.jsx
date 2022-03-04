af846c8363b3afd24a13ec68180151af6adffcc9
Adiazx14 <adrianvaldes1439@gmail.com>
2022-03-03 23:46:19 -0500

    finish global winning animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 
@@ -8,23 +8,38 @@ function App() {
     [0, 0,  0]])
   const [turn, setTurn] = useState(true)
   
+  const [mainClass, setMainClass] = useState("")
+  
+ 
   const checkWinner = () => {
        
-  if ((matrix[0][0] !== 0) && ((matrix[0][0] === matrix[0][1] && matrix[0][0] === matrix[0][2]) || (matrix[0][0] === matrix[1][0] && matrix[0][0] === matrix[2][0]) || (matrix[0][0] === matrix[1][1] && matrix[0][0] === matrix[2][2]))) {
-      return true
+  if (matrix[0][0] !== 0) {
+    if (matrix[0][0] === matrix[0][1] && matrix[0][0] === matrix[0][2]) {
+      setMainClass("ltor-top")
+    } 
+    if (matrix[0][0] === matrix[1][0] && matrix[0][0] === matrix[2][0]) {
+      setMainClass("ttob-left")
+    } 
+    if (matrix[0][0] === matrix[1][1] && matrix[0][0] === matrix[2][2]) {
+      setMainClass("ltor-corner")
     }
-  if ((matrix[0][1] !== 0) && (matrix[0][1] === matrix[1][1] && matrix[0][1] === matrix[2][1])) {
-
-    return true
   }
-  if ((matrix[0][2] !== 0) && ((matrix[0][2] === matrix[1][2] && matrix[0][2] === matrix[2][2]) || (matrix[0][2] === matrix[1][1] && matrix[0][2] === matrix[2][0]))) {
-    return true
+  if ((matrix[0][1] !== 0) && (matrix[0][1] === matrix[1][1] && matrix[0][1] === matrix[2][1])){
+    setMainClass("ttob-middle")
+  }
+  if (matrix[0][2] !== 0) {
+    if (matrix[0][2] === matrix[1][2] && matrix[0][2] === matrix[2][2]) {
+      setMainClass("ttob-right")
+    }
+    if (matrix[0][2] === matrix[1][1] && matrix[0][2] === matrix[2][0]) {
+      setMainClass("rtol-corner")
+  }
   }
   if (matrix[1][0] !== 0 && (matrix[1][0] === matrix[1][1] && matrix[1][0] === matrix[1][2])) {
-    return true
+    setMainClass("ltor-middle")
   }
   if (matrix[2][0] !== 0 && (matrix[2][0] === matrix[2][1] && matrix[2][0] === matrix[2][2])) {
-    return true
+    setMainClass("ltor-bottom")
   }
   return 0
   }
@@ -51,7 +66,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-      <main className='ltor-corner'>
+      <main className={mainClass}>
         {matrix.map((level, row)=>level.map((cell, column)=><Board turn={turn} setTurn={setTurn} checkMainTie={checkTie} checkMainWinner={checkWinner} index={[row, column]} mainMatrix={matrix} changeMatrix={setMatrix} key={row*3+column}/>))}
       </main>
     </div>
